0657dc336bbcbd437e9de9e16c0aabd5bcebbc94
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-17 06:44:34 +0600

    random ....ludo

---
+++ random.js
@@ -0,0 +1,20 @@
+// var num =2.67569;
+// var result = Math.floor(num);
+// var result2 = Math.ceil(num);
+// var result3 = Math.round(num);
+//  var random1 = Math.random() ;
+// var random2 = Math.random()*6;
+// var output = Math.round(random2);
+// console.log(result);
+// console.log(result2);
+// console.log(result3);
+//  console.log(random1); 
+// console.log(random2);
+// console.log(output);
+
+for(var i=0; i<10; i++){
+    var random2 = Math.random()*6;                //ludoooo
+    var output = Math.round(random2);
+    console.log(output);
+}
+
