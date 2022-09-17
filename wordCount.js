4ddface0fd079ecc481803a4fc1127726bfd18f3
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-17 07:57:05 +0600

    word count

---
+++ wordCount.js
@@ -0,0 +1,12 @@
+var speech = "i am a student. my   name is    rabiul islam.";
+console.log(speech.length);
+var count =0;
+for(var i=0; i<speech.length; i++){
+    var char = speech[i];
+    // console.log(char);
+    if (char==" " && speech[i-1]  !=" "){
+        count++;
+    }
+}
+count++;
+console.log(count);
