d973ede7f07898afa86a1f38350497bf7ac6e7c8
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-17 07:19:48 +0600

    sum array element normally

---
+++ arraySum.js
@@ -0,0 +1,8 @@
+var marks =[52,25,31,48,67,5,79,52,54,54,67,90,8,47,46];
+var sum =0;
+for (var i=0; i<marks.length; i++){
+    var element = marks[i];
+    sum= sum + element;
+    
+}
+console.log('sum of array elements is:',sum);
