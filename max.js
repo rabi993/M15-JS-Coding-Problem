acec05053827512359c3a5d2d4a685883c9f49ac
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-17 07:01:45 +0600

    max done

---
+++ max.js
@@ -0,0 +1,42 @@
+var a=40;
+var b=70;
+var c=60;
+
+if(a>b){
+    console.log('a is max');
+}
+if (a==b){
+    console.log('a & b are equal');
+}
+else{
+    console.log('b is max');
+}
+
+console.log('shdfjkljkhggggggggggggggggggggg');
+
+if(a>b){
+    if(a>c){
+        console.log('a is max');
+    }
+    else{
+        console.log('c is max');
+    }
+    
+}
+else{
+    if(b>c){
+            console.log('b is max');
+        }
+        else{
+            console.log('c is max');
+        }
+        
+    }
+
+
+
+console.log('shdfjkljkhgggggggggggggggtyjtrfjgggggg');
+
+
+var max =Math.max(a,b,c);
+console.log(max);
