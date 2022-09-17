c6a7d42c865e0135381940a2c3501e9836872c3c
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-17 08:19:57 +0600

    ......

--- wordCount.js
+++ wordCount.js
@@ -1,3 +1,31 @@
+var speech = "i am a student. my name is rabiul islam.";
+console.log(speech.length);
+var count =0;
+for(var i=0; i<speech.length; i++){
+    var char = speech[i];
+    // console.log(char);
+    if (char==" " && speech[i-1]  !=" "){
+        count++;
+    }
+}
+count++;
+console.log(count);
+
+
+var speech = "i am a student. my name is rabiul islam. ";
+console.log(speech.length);
+var count =0;
+for(var i=0; i<speech.length; i++){
+    var char = speech[i];
+    // console.log(char);
+    if (char==" " && speech[i-1]  !=" "){
+        count++;
+    }
+}
+count++;
+console.log(count);
+
+
 var speech = "i am a student. my   name is    rabiul islam.";
 console.log(speech.length);
 var count =0;
