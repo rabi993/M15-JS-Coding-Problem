209fc7d85143b2ce94bad46a57c21e61a338d839
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-17 07:28:44 +0600

    sum of array element by function

--- arrayMax.js
+++ arrayMax.js
@@ -1,4 +1,4 @@
-var marks =[52,25,31,48,67,5,79,52,54,54,67,90,8,47,46];
+var marks =[52,25,31,48,67,5,79,52,54,54,67,90,5,47,46];
 var max =marks[0];
 for (var i=0; i<marks.length; i++){
     var element =marks[i];
--- arraySum.js
+++ arraySum.js
@@ -1,8 +1,22 @@
-var marks =[52,25,31,48,67,5,79,52,54,54,67,90,8,47,46];
-var sum =0;
-for (var i=0; i<marks.length; i++){
-    var element = marks[i];
-    sum= sum + element;
+// var marks =[52,25,31,48,67,5,79,52,54,54,67,90,8,47,46];
+// var sum =0;
+// for (var i=0; i<marks.length; i++){
+//     var element = marks[i];
+//     sum= sum + element;
     
+// }
+// console.log('sum of array elements is:',sum);
+
+
+function getArraySum(marks){
+    var sum =0;
+    for (var i=0; i<marks.length; i++){
+        var element = marks[i];
+        sum= sum + element;
+        
+    }
+    return  sum;
 }
-console.log('sum of array elements is:',sum);
+var marks =[52,25,31,48,67,5,79,52,54,54,67,90,8,47,46];
+var result = getArraySum(marks);
+console.log('sum of array elements is:',result);
