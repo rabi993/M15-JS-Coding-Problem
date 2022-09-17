1428f78022d70cd5b2b675b01690652d63610a82
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-17 06:05:35 +0600

    start solving

---
+++ swap.js
@@ -0,0 +1,28 @@
+var a =4;
+var b =9;
+console.log('before swap: a=', a, 'b= ', b );
+var temp=b;
+var b=a;
+var a=temp;
+console.log('after swap: a=', a, 'b= ', b );
+
+// var a =4;
+// var b =9;
+// // console.log('before swap: a=', a, 'b= ', b );
+// var temp = a;
+//  a = b;
+//  b = temp;
+// console.log('after swap: a=', a, 'b= ', b );
+
+var x=7;
+var y=8;
+x=x+y;
+y=x-y;
+x=x-y;
+console.log('after swap: x=', x, 'y= ', y );
+// dhfjgkhljki;olkojjdsafkl;kjdsadfkl;jdsafghj
+
+var p=5;
+var q=7;
+[p,q] = [q,p];
+console.log('after swap: p=', p, 'q= ', q );
