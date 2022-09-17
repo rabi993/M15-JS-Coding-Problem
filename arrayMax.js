579cb5fff26456c2862036709b5d31c490099767
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-17 07:09:57 +0600

    max array element

---
+++ arrayMax.js
@@ -0,0 +1,11 @@
+var marks =[52,25,31,48,67,5,79,52,54,54,67,90,5,47,46];
+var max =marks[0];
+for (var i=0; i<marks.length; i++){
+    var element =marks[i];
+    if(element>max){
+        max= element;
+       
+    }
+    
+}
+console.log('max value is:',max);
